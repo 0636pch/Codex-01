360f025907539553aa410b0b68ce86c1659774fc
0636pch <316836496+0636pch@users.noreply.github.com>
2026-09-01 15:42:12 +0900

    feat: add body_quest/lib/main.dart

---
+++ body_quest/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app.dart';
+
+const useFirebase = bool.fromEnvironment('USE_FIREBASE');
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (useFirebase) {
+    await Firebase.initializeApp();
+  }
+  runApp(const ProviderScope(child: BodyQuestApp()));
+}

82b879d3e3436e3e470c6c9f9ce38f0f4f4594d2
0636pch <316836496+0636pch@users.noreply.github.com>
2026-09-01 23:23:55 +0900

    feat: extend onboarding authentication contract

--- body_quest/lib/features/onboarding/data/onboarding_repository.dart
+++ body_quest/lib/features/onboarding/data/onboarding_repository.dart
@@ -1,7 +1,12 @@
 import '../domain/onboarding_data.dart';
 
 abstract interface class OnboardingRepository {
+  Future<String> resolveStartRoute();
   Future<void> createAccount(String email, String password);
+  Future<void> signIn(String email, String password);
+  Future<void> sendPasswordReset(String email);
+  Future<void> saveConsents({required bool marketing});
+  Future<void> signOut();
   Future<void> saveProfile(ProfileInput input);
   Future<void> saveInbody(InbodyInput? input);
   Future<void> saveCharacter(CharacterInput input);
